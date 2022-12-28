1017160d57591e98287bd57fb2a18097ee2cd854
chin84 <chin84@inbox.ru>
2022-12-28 13:00:36 +0300

    Update Ya.py
    fixed

--- Ya.py
+++ Ya.py
@@ -2,43 +2,51 @@ from pprint import pprint
 import requests
 
 
-def get_token():
-    with open('токен для яндекс.txt', 'r') as f:
-        return f.readlines()
+
+class YaUploader:
+    files_url = 'https://disk.yandex.ru/client/disk/files/Текст для яндекса.txt'
+    upload_link = 'https://cloud-api.yandex.net/v1/disk/resources/upload?path=%D0%A2%D0%B5%D0%BA%D1%81%D1%82%20%D0%B4%D0%BB%D1%8F%20%D1%8F%D0%BD%D0%B4%D0%B5%D0%BA%D1%81%D0%B0&url=https%3A%2F%2Fdisk.yandex.ru%2Fclient%2Fdisk%2Ffiles%2F%D0%A2%D0%B5%D0%BA%D1%81%D1%82%20%D0%B4%D0%BB%D1%8F%20%D1%8F%D0%BD%D0%B4%D0%B5%D0%BA%D1%81%D0%B0.txt'
 
 
-TOKEN = get_token()
-for token in TOKEN:
-    headers = {
-                'Content-Type': 'application/json',
-                'Authorization': f"OAuth {token}"
-
-              }
-
-files_url = 'https://disk.yandex.ru/client/disk/files/Текст для яндекса.txt'
-upload_link = 'https://cloud-api.yandex.net/v1/disk/resources/upload?path=%D0%A2%D0%B5%D0%BA%D1%81%D1%82%20%D0%B4%D0%BB%D1%8F%20%D1%8F%D0%BD%D0%B4%D0%B5%D0%BA%D1%81%D0%B0&url=https%3A%2F%2Fdisk.yandex.ru%2Fclient%2Fdisk%2Ffiles%2F%D0%A2%D0%B5%D0%BA%D1%81%D1%82%20%D0%B4%D0%BB%D1%8F%20%D1%8F%D0%BD%D0%B4%D0%B5%D0%BA%D1%81%D0%B0.txt'
+    def __init__(self, token: str):
+        self.token = token
 
 
-def get_upload_link(file_path):
-    params = {'path': file_path, 'overwrite': 'true'}
-    response = requests.get(upload_link, params=params, headers=headers)
-    pprint(response.json())
-    return response.json()
+    @property
+    def headers(self):
+        return {
+            'Content-Type': 'application/json',
+            'Authorization': f"OAuth {token}"
+                  }
 
 
-def upload(file_path):
-    href = get_upload_link(file_path).get('href')
-    if not href:
-        return
 
-    with open(file_path, 'rb') as file:
-        response = requests.put(href, data=file)
-        if response.status_code == 201:
-            print('File uploaded successfully')
-            return True
-        else:
-            print('File upload failed', response.status_code)
+    def get_upload_link(self, file_path):
+        params = {'path': file_path, 'overwrite': 'true'}
+        response = requests.get(self.upload_link, params=params, headers=self.headers)
+        pprint(response.json())
+        return response.json()
+
+
+
+    def upload(self, file_path ):
+        href = self.get_upload_link(file_path).get('href')
+        if not href:
+            return
+
+        with open(file_path, 'rb') as file:
+            response = requests.put(href, data=file)
+            if response.status_code == 201:
+                print('File uploaded successfully')
+                return True
+            else:
+                print('File upload failed', response.status_code)
 
 
 if __name__ == '__main__':
-    print(upload(file_path='Текст для яндекса.txt'))
+    path_to_file = 'Текст для яндекса.txt'
+    token = 'y0_AgAAAABkPyMuAAjz_AAAAADX-BHtmNnR110HT_aSuRyeSFtWWNjUTf0'
+    uploader = YaUploader(token)
+    result = uploader.upload(path_to_file)
+    print(result)
+
