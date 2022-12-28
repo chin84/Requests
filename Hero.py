198a03df122f5c29324fee0768827cc5b1cf3ffe
chin84 <chin84@inbox.ru>
2022-12-28 12:10:28 +0300

    Update Hero.py
    Fixed

--- Hero.py
+++ Hero.py
@@ -1,36 +1,22 @@
+import sys
 import requests
-import random
-
-url = requests.get('https://akabab.github.io/superhero-api/api/all.json')
-jsoni_url = url.json()
 
 
-def iq_hero(name):
-    name_lst = []
-    stats_lst = []
-    for nam in jsoni_url:
-        for value, stats in nam.items():
-            if value == 'name':
-                name_lst.append(stats)
-        for mind, intel in nam['powerstats'].items():
-            if mind == 'intelligence':
-                stats_lst.append(intel)
-    heroes_dict = dict(zip(name_lst, stats_lst))
-    return f'Самый умный герой {name} с интеллектом {heroes_dict[name]}'
+def best_hero(heroes_list:['Hulk', 'Capitan America', 'Thanos']):
+    heroes_dict = {}
+    url ='https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/all.json'
+    response = requests.get(url)
+    if response.status_code != 200:
+        sys.exit('К сожалению в запросе ошибка')
+    jsonify_response = response.json()
+    for hero in jsonify_response:
+        heroes_lst = ['Hulk', 'Capitan America', 'Thanos']
+        if hero['name'] in heroes_lst:
+            heroes = int(hero['powerstats']['intelligence'])
+            heroes_dict = hero['name']
+    result = f"\nСамый умный супергерой: {heroes_dict} - его интелект: {heroes}"
+    return result
 
-
-def max_intellect():
-    name_lst = []
-    for nam in jsoni_url:
-        for value, stats in nam.items():
-            if value == 'name':
-                name_lst.append(stats)
-    return name_lst
-
-
-choice_hero = random.choice(max_intellect())
-result = iq_hero(choice_hero), iq_hero(choice_hero), iq_hero(choice_hero)
-max_result = max(result)
 
 if __name__ == "__main__":
-    print(max_result)
+     print(best_hero(['Hulk', 'Captain America', 'Thanos']))
