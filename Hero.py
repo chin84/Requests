4dc0498548fa111328debeef8104695a98b51d0b
chin84 <chin84@inbox.ru>
2022-12-28 20:48:31 +0300

    Update Hero.py

--- Hero.py
+++ Hero.py
@@ -2,7 +2,7 @@ import sys
 import requests
 
 
-def best_hero(heroes_list:['Hulk', 'Capitan America', 'Thanos']):
+def best_hero(heroes_list):
     heroes_dict = {}
     url ='https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/all.json'
     response = requests.get(url)
@@ -10,12 +10,11 @@ def best_hero(heroes_list:['Hulk', 'Capitan America', 'Thanos']):
         sys.exit('К сожалению в запросе ошибка')
     jsonify_response = response.json()
     for hero in jsonify_response:
-        heroes_lst = ['Hulk', 'Capitan America', 'Thanos']
-        if hero['name'] in heroes_lst:
-            heroes = int(hero['powerstats']['intelligence'])
-            heroes_dict = hero['name']
-    result = f"\nСамый умный супергерой: {heroes_dict} - его интелект: {heroes}"
-    return result
+        name = hero['name']
+        if name in heroes_list:
+            heroes_dict[name] = int(hero['powerstats']['intelligence'])
+    result = max(heroes_dict.items(), key=lambda x: x[1])
+    return f'Самый умный супер герой {result[0]} - его интеллект {result[1]}'
 
 
 if __name__ == "__main__":
